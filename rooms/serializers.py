246845e6889b8880d3d2356171df69e4ca94b835
Oleksii Ivanov <pcharm563@gmail.com>
2022-12-28 18:56:04 +0100

    add post room-reviews

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
+from medias.serializers import PhotoSerializer
 
 
 class AmenitySerializer(serializers.ModelSerializer):
@@ -21,6 +22,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     )
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
         model = Room
@@ -38,6 +40,7 @@ class RoomListSerializer(serializers.ModelSerializer):
 
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    photos = PhotoSerializer(many=True, read_only=True)
 
     class Meta:
         model = Room
@@ -49,6 +52,7 @@ class RoomListSerializer(serializers.ModelSerializer):
             "price",
             "rating",
             "is_owner",
+            "photos",
         )
 
     def get_rating(self, room):
